de72f625b4c8e65b7e4dd80b08c42683a5af12a7
Kainoa Nishida <kanishid@uci.edu>
2024-01-28 02:40:33 -0800

    backend

--- backend/express-locallibrary-tutorial/app.js
+++ backend/express-locallibrary-tutorial/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
+const profileRouter = require('./routes/profileRouter');
+
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter)
+app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
